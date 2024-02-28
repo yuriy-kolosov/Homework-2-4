e2e32ce6b1c161ae41501d695280c98e99f27887
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-02-28 18:07:05 +0300

    Домашнее задание 2-4. Введение в Maven и Spring Boot - начальный коммит

---
+++ src/main/java/pro/sky/homework_2_4/JavaSkyProHomeworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homework_2_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSkyProHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSkyProHomeworkApplication.class, args);
+	}
+
+}
